5a6bb757810052e58afc238a8d8b4159375a1e41
Pavlo Brychka <brychkapavlo.work@gmail.com>
2023-02-26 18:20:43 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'an_app_0_Ekz';
+}
